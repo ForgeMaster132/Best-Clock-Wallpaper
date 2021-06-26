d2108741a60ecf7fcea6a923ec5d8828af9973d3
Dayle Klinkhamer <46386248+ForgeMaster132@users.noreply.github.com>
2021-06-26 02:47:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+// Getting new color values function
+function newColors(){
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// Getting RGB values at random with function call    
+let newColor = getRandomInt(1, 255);
+return newColor;
+}
+
+// Find Difference between colors
+function findDiff(oldVal, newVal){
+    return ((newVal - oldVal)/60);
+}
+
+// Main Clock Function
+function updateClock() {
+    // Do Once to start clock
+    let newRed = newColors();
+    let newGreen = newColors();
+    let newBlue = newColors();
+
+    let redInterval = findDiff(newRed,newColors());
+    let greenInterval = findDiff(newGreen,newColors());
+    let blueInterval = findDiff(newBlue, newColors());
+
+    for(let i = 1; i <= 60; i++){        
+        // Seconds
+        setTimeout(function() {
+        var currentTime = new Date();
+        // Operating System Clock Hours for 24h clock
+        var currentHours = currentTime.getHours();
+        // Operating System Clock Minutes
+        var currentMinutes = currentTime.getMinutes();
+        // Operating System Clock Seconds
+        var currentSeconds = currentTime.getSeconds();
+        // Adding 0 if Minutes & Seconds is More or Less than 10
+        currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+        currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+        // display first 24h clock and after line break 12h version
+        var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+        // print clock js in div #clock.
+        document.getElementById("clock").innerHTML = currentTimeString;
+        
+        newRed += redInterval;
+        newGreen += greenInterval;
+        newBlue += blueInterval;
+
+        document.body.style.backgroundColor = 'rgb(' + newRed + ',' + newGreen + ',' + newBlue + ')';
+
+    }, 1000 * i);
+    }
+    
+    ////////
+    
+
+    // Continues Clock Every Minute
+    // Minutes
+    setInterval(function() {
+
+       redInterval = findDiff(newRed,newColors());
+       greenInterval = findDiff(newGreen,newColors());
+       blueInterval = findDiff(newBlue, newColors());
+
+        for(let i = 1; i <= 60; i++){        
+            // Seconds
+            setTimeout(function() {
+            var currentTime = new Date();
+            // Operating System Clock Hours for 24h clock
+            var currentHours = currentTime.getHours();
+            // Operating System Clock Minutes
+            var currentMinutes = currentTime.getMinutes();
+            // Operating System Clock Seconds
+            var currentSeconds = currentTime.getSeconds();
+            // Adding 0 if Minutes & Seconds is More or Less than 10
+            currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+            currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+            // display first 24h clock and after line break 12h version
+            var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
+            // print clock js in div #clock.
+            document.getElementById("clock").innerHTML = currentTimeString;
+            
+            newRed += redInterval;
+            newGreen += greenInterval;
+            newBlue += blueInterval;
+
+            document.body.style.backgroundColor = 'rgb(' + newRed + ',' + newGreen + ',' + newBlue + ')';
+        }, 1000 * i);
+    }
+}, 60000);
+};
+
+updateClock();
